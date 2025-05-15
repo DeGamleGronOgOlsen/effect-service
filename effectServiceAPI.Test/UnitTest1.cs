0708660874013e67520d3aa291ac590e69778a01
RasmusOlesen00 <146640318+RasmusOlesen00@users.noreply.github.com>
2025-05-15 18:50:00 +0200

    Initial commit

---
+++ effectServiceAPI.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace effectServiceAPI.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
