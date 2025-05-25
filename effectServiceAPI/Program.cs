b5d621217692d64e5c8237b4d09d73cb5574f9ca
RasmusOlesen00 <146640318+RasmusOlesen00@users.noreply.github.com>
2025-05-25 14:55:55 +0200

    Loki

--- effectServiceAPI/Program.cs
+++ effectServiceAPI/Program.cs
@@ -1,70 +1,89 @@
 using auctionServiceAPI.Services;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Net.Http.Headers;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("start min service");
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddHttpClient();
-builder.Services.AddControllers();
-builder.Services.AddSingleton<MongoDBContext>();
-builder.Services.AddScoped<IEffectService, EffectMongoDBService>();
-
-// Configure CORS policy to allow your frontend
-builder.Services.AddCors(options =>
+try
 {
-    options.AddPolicy("AllowFrontend", policy =>
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    // Add services to the container.
+    builder.Services.AddRazorPages();
+    builder.Services.AddHttpClient();
+    builder.Services.AddControllers();
+    builder.Services.AddSingleton<MongoDBContext>();
+    builder.Services.AddScoped<IEffectService, EffectMongoDBService>();
+
+    // Configure CORS policy to allow your frontend
+    builder.Services.AddCors(options =>
     {
-        policy.WithOrigins("http://localhost:8080")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
+        options.AddPolicy("AllowFrontend", policy =>
+        {
+            policy.WithOrigins("http://localhost:8080")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
     });
-});
 
-// Create globally availabel HttpClient for accesing the gateway.
-var gatewayUrl = builder.Configuration["GatewayUrl"] ?? "http://localhost:4000";
-builder.Services.AddHttpClient("gateway", client =>
-{
-    client.BaseAddress = new Uri(gatewayUrl);
-    client.DefaultRequestHeaders.Add(
-        HeaderNames.Accept, "application/json");
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-// Make sure these services are registered in Program.cs
+    // Create globally available HttpClient for accessing the gateway.
+    var gatewayUrl = builder.Configuration["GatewayUrl"] ?? "http://localhost:4000";
+    builder.Services.AddHttpClient("gateway", client =>
+    {
+        client.BaseAddress = new Uri(gatewayUrl);
+        client.DefaultRequestHeaders.Add(
+            HeaderNames.Accept, "application/json");
+    });
 
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
+    var app = builder.Build();
 
-var app = builder.Build();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // CORS middleware must be early in the pipeline
+    app.UseCors("AllowFrontend");
+
+    app.UseStaticFiles();
+
+    var imagePath = builder.Configuration["ImagePath"];
+    var fileProvider = new PhysicalFileProvider(Path.GetFullPath(imagePath));
+    var requestPath = new PathString("/images/effect");
+    app.UseStaticFiles(new StaticFileOptions()
+    {
+        FileProvider = fileProvider,
+        RequestPath = requestPath
+    });
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.MapRazorPages();
+
+    app.Run();
 }
-// CORS middleware must be early in the pipeline
-app.UseCors("AllowFrontend");
-
-app.UseStaticFiles();
-
-var imagePath = builder.Configuration["ImagePath"];
-var fileProvider = new PhysicalFileProvider(Path.GetFullPath(imagePath));
-var requestPath = new PathString("/images/effect");
-app.UseStaticFiles(new StaticFileOptions()
+catch (Exception ex)
 {
-    FileProvider = fileProvider,
-    RequestPath = requestPath
-});
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapRazorPages();
-
-app.Run();
+    logger.Error(ex, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    NLog.LogManager.Shutdown();
+}
